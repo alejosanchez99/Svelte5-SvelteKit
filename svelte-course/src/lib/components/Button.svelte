aa805b55764cdabd54e51b8cc8948d16849f1d87
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-06-30 15:54:00 -0500

    Global selector and class directives.

---
+++ svelte-course/src/lib/components/Button.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	type Props = HTMLButtonAttributes & {
+		left?: Snippet<[boolean]>;
+		right?: Snippet;
+		children: Snippet<[boolean]>;
+		size?: 'sm' | 'lg';
+		shadow?: boolean;
+		bgColor?: string;
+		textColor?: string;
+	};
+	let {
+		left,
+		right,
+		size = 'sm',
+		shadow = false,
+		children,
+		bgColor,
+		textColor,
+		...props
+	}: Props = $props();
+
+	let isLeftHovered = $state(false);
+</script>
+
+<!-- <button class={{ ['sm other-class']: size === 'sm', lg: size === 'lg', shadow }}> -->
+<button
+	class={[size === 'sm' && 'sm', size === 'lg' && 'lg', shadow && 'shadow']}
+	style:--buttonBgColor={bgColor}
+	style:--buttonTextColor={textColor}
+	{...props}
+>
+	{#if left}
+		<div
+			role="presentation"
+			class="left-content"
+			onmouseenter={() => (isLeftHovered = true)}
+			onmouseleave={() => (isLeftHovered = false)}
+		>
+			{@render left(isLeftHovered)}
+		</div>
+	{/if}
+	{@render children(isLeftHovered)}
+	{#if right}
+		<div class="right-content">
+			{@render right()}
+		</div>
+	{/if}
+</button>
+
+<style lang="scss">
+	button {
+		border: none;
+		background-color: var(--buttonBgColor, #ff3e00);
+		color: var(--buttonTextColor, #ffffff);
+		padding: 0 20px;
+		height: 45px;
+		font-weight: bold;
+		border-radius: 5px;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
+		}
+		&:hover {
+			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
+		}
+		&:active {
+			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+		}
+		&.sm {
+			height: 45px;
+		}
+		&.lg {
+			height: 55px;
+			font-size: 20px;
+		}
+		&.shadow {
+			box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
+		}
+		.left-content {
+			margin-inline-end: 10px;
+			:global(svg) {
+				stroke: red;
+			}
+		}
+		.right-content {
+			margin-inline-start: 10px;
+		}
+	}
+</style>
--- svelte-course/src/routes/+page.svelte
+++ svelte-course/src/routes/+page.svelte
@@ -1,10 +1,48 @@
-<script>
-	import Counter from '$lib/components/Counter.svelte';
-	import DisplayName from '$lib/components/DisplayName.svelte';
-	import RandomNumber from '$lib/components/RandomNumber.svelte';
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	import { AlarmCheck, Search } from 'lucide-svelte';
 </script>
 
 <!-- <h1>Outer</h1>
 <DisplayName /> -->
 <!-- <RandomNumber /> -->
-<Counter />
+<!-- <Counter /> -->
+
+<div class="wrapper">
+	<Button size="sm" bgColor="green" textColor="yellow">
+		{#snippet left(isHovered: boolean)}
+			{#if isHovered}
+				<Search />
+			{:else}
+				<AlarmCheck />
+			{/if}
+		{/snippet}
+		Text
+		<!-- {#snippet children(isLeftHovered)}
+		Text {isLeftHovered}
+	{/snippet} -->
+		{#snippet right()}
+			<AlarmCheck />
+		{/snippet}
+	</Button>
+</div>
+
+<style>
+	:global {
+		body {
+			background: #222;
+		}
+	}
+	.wrapper :global {
+		button {
+			background-color: blue;
+		}
+	}
+
+	/* :global(body) {
+		background-color: #222;
+	} 
+	.wrapper :global(button) {
+		background-color: blue;
+	} */
+</style>
