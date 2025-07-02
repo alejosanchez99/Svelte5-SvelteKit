cb1abe26cf2fc9b52477440c224265f2fd823443
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-07-01 19:33:30 -0500

    Proxy javascript.

---
+++ svelte-course/src/routes/reactivity/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	const target = {
+		firstName: 'Ali',
+		lastName: 'Alaa',
+		occupations: [],
+		get fullName() {
+			return `${this.firstName} ${this.lastName}`;
+		},
+		set occupation(value: string) {
+			console.log(`Adding ${value} to occupations`);
+			this.occupations.push(value);
+		}
+	};
+
+	target.firstName = 'Omar';
+	target.occupation = 'Web developer';
+
+	const handler = {
+		get(target, prop) {
+			console.log(target, prop);
+			return prop in target ? target[prop] : 'NA';
+		},
+		set(target, prop, value) {
+			if (['firstName', 'lastName'].includes(prop)) {
+				if (typeof value !== 'string') {
+					throw new TypeError(`Property ${prop} must be a string.`);
+				}
+			}
+			target[prop] = value;
+			return true;
+		}
+	};
+	const proxy = new Proxy(target, handler);
+	proxy.firstName = 'Omar';
+
+	console.log(proxy.firstName);
+</script>
+
+<style>
+	:global {
+		body {
+			background-color: #222;
+		}
+	}
+</style>
